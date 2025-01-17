5ea4a28f082d00d47e9cfb490497101a290c2383
Viktoriiap <viktoriiap0205199@gmail.com>
2025-01-17 13:17:10 +0100

    feat: add 2-nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,39 @@
+const fs = require ('fs');
+const readLine = require ('readline');
+const text = 'text.txt';
+let isSigint = false;
+
+const userUnput = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+const createFile = async () => {
+    await fs.writeFile (text,'', (err)=>{console.log(err)});
+}
+const safeIntFile = async (input) => {
+    await fs.appendFile(text, input + ' ', (err)=>{console.log(err)});
+    };
+
+
+ const main = async() => {
+    await createFile ();
+    console.log ("Please, inter anything or inter 'exit' for going out:");
+    userUnput.on ('line', async (input) => {
+        if (input.trim().toLowerCase() === 'exit' || isSigint) {
+            console.log ("Writing is ended")
+            userUnput.close();
+            isSigint = false;
+        } else {
+            await safeIntFile(input);
+        }
+    })
+ }
+
+ main().catch(err =>
+    console.error('incorrect', err)
+ );
+    
+    
+ 
