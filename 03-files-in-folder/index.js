1a3ccfb43fb31d9860cc6cabb29064f91db74422
Viktoriiap <viktoriiap0205199@gmail.com>
2025-01-17 15:41:47 +0100

    feat: add 3-th task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs/promises');
+const path = require('path');
+const dirPath = './secret-folder';
+
+async function readFolderFiles (folder) {
+    try {
+        const dirents = await fs.readdir(folder, {withFileTypes:true});
+        for (const dirent of dirents) {
+            if(dirent.isFile()) {
+                const fileName = dirent.name;
+                const filePath = path.join(folder, fileName);
+                const fileStats = await fs.stat(filePath);
+                const fileExtension = path.extname(fileName)
+                const fileSize = fileStats.size;
+                const NewFileName = path.basename(fileName, fileExtension)
+
+                console.log (`${NewFileName} ${fileExtension} ${fileSize}`);
+            }
+        }
+    } catch (error) {
+        console.error('Error,', error);
+    }
+}
+readFolderFiles(dirPath)
